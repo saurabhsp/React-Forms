955a522ec45859a3759063fd6f598427fa7aff77
saurabhsp <smpakhale36@gmail.com>
2024-07-19 19:02:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [pswd, setpswd] = useState("");
+  function handleChange(e) {
+    console.log(e.target.name)
+    if(e.target.name === "fname"){
+      const capName= (e.target.value).toUpperCase();
+      setName(capName);
+  }
+  else{
+    setpswd(e.target.value)
+  }
+}
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <label htmlFor="name">Name</label>
+        <br />
+        <input type="text" name="fname" value={name} onChange={handleChange}></input>
+        <br />
+        <label htmlFor="password">Password</label>
+        <br />
+        <input type="text" name="pswd" value={pswd} onChange={handleChange}></input>
+        <br />
+      </div>
+    </>
   );
 }
 
